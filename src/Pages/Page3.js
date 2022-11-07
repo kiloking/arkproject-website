d9d0f028e907bc45c7f459e48bb14e72488b73b5
wood.wu <kilokingw@gmail.com>
2022-11-07 15:48:36 +0800

    Update Page3.js

--- src/Pages/Page3.js
+++ src/Pages/Page3.js
@@ -10,7 +10,7 @@ function Page3() {
         <img src={process.env.PUBLIC_URL+'/images/page3_p01_mb.png'} alt="" className='w-full'/>
       </div>
       <div className='lg:w-[55%] order-first lg:order-last'>
-        <div className='lg:bg-[#E7ECEF] lg:px-[10%] lg:py-[18%] lg:py-[10%] px-[10%] ' data-aos="fade" data-aos-duration="1500">
+        <div className='lg:bg-transparent lg:px-[10%] lg:py-[18%] lg:py-[10%] px-[10%] ' data-aos="fade" data-aos-duration="1500">
           <div className='w-full lg:w-3/5 mx-auto'>
             {/* <img src={process.env.PUBLIC_URL+'/images/page3_title.png'} alt="" className='max-w-full mb-16'/> */}
             <div className='text-3xl lg:text-4xl  font-bold text-[#20494C] border-l-[12px] border-[#B9D1DC] lg:border-white pt-5 pb-5 pl-[7%] mb-5'>重新大道 繁華盛景</div>
