037d08f7a35ed6b2b22c2632a24c3db9e130f459
wood.wu <kilokingw@gmail.com>
2023-03-30 11:32:31 +0800

    updated news cover img

--- src/Pages/Press.js
+++ src/Pages/Press.js
@@ -12,7 +12,7 @@ function Press() {
         </a>
         <a href="https://tw.news.yahoo.com/%E5%86%A0%E5%BE%B7%E6%90%B6%E9%80%B2%E4%B8%89%E9%87%8D-%E7%B7%8A%E9%84%B0%E6%8D%B7%E9%81%8B%E7%AB%99-201000712.html" className='transition-all hover:brightness-110' target="_blank" data-aos="fade-up" data-aos-duration="1500">
             <div className=' aspect-[385/209] bg-black  bg-center bg-cover bg-no-repeat'
-                style={{backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg)`}}
+                style={{backgroundImage: `url(${process.env.PUBLIC_URL+'/images/news_cover02.png'})`}}
             ></div>
           <div className='text-[#20494C] text-base font-bolder mt-2'>冠德搶進三重 緊鄰捷運站</div>
         </a>
